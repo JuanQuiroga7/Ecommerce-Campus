4706af03c4d95e1684b7999adbc6112191ce995e
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-06-19 19:21:09 -0500

    feat: :sparkles: Agregar informacion del producto seccion detalle
    Se realiza estructuracion HTML y se configura JS para visualizcion de informacion del detalle del producto

--- js/detail.js
+++ js/detail.js
@@ -1,13 +1,18 @@
 import { galleryCategory } from "./components/gallery.js";
 import { getProductId } from "./module/detail.js";
+import { titleProductDetail } from "./components/section.js";
 
-let main__section_gallery = document.querySelector("#main__section_gallery");
+let main__section_gallery = document.querySelector("#main__section__gallery");
+let main__section__title = document.querySelector("#main__section__title");
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
-    main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
+    let info = JSON.parse(localStorage.getItem(id));
+    main__section_gallery.innerHTML = await galleryCategory(info);
+    main__section__title.innerHTML = await titleProductDetail(info);
+    
     // let {data} = res;
     // let {
     //     category_path,
@@ -22,3 +27,4 @@ addEventListener("DOMContentLoaded", async(e)=>{
     // } = data;
     // console.log(dataUpdate);
 })
+
