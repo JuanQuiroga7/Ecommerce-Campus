bb9473470d668215b91739d7dfaab09f70e1aa52
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-06-19 21:26:02 -0500

    feat: :sparkles: Muestra de producto random
    Se crea estrucutra para mostrar en pagina principal lista de productos al azar para mantener el inicio cargado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { menuListCategoryIndex } from "./components/menu.js";
 import { galleryIndex } from "./components/gallery.js";
-import { getAllProductName, getAllCategory } from "./module/app.js";
+import { getAllProductName, getAllCategory, getAllProductRandom } from "./module/app.js";
+import { getProductId } from "./module/detail.js";
+
 
 
 
@@ -9,19 +11,49 @@ let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main_article");
 let nav__ul = document.querySelector(".nav__ul");
 
-addEventListener("DOMContentLoaded", async e=>{
-    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
-    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));    
-})
+let searchProducts = async e => {
 
-input__search.addEventListener("change", async e => {
     let params = new URLSearchParams(location.search);
-    let data = { search : e.target.value, id: params.get('id')}
+    let dataSearch = { search : e.target.value, id: params.get('id')}
     input__search.value = null;
-
-    let res = await getAllProductName(data)
+    let res = ""
+    if(input__search.dataset.opc == "random"){
+        res = await getAllProductRandom({})
+        delete input__search.dataset.opc
+        history.pushState(null, "", "?id=aps");
+        console.log(dataSearch);
+    }
+    else {
+        res = await getAllProductName(dataSearch)
+        console.log(dataSearch);
+    }
+    console.log(res);
     main__article.innerHTML = galleryIndex(res, params.get('id'));
 
+    let {data: {products}} = res;
+    let asin = products.map(value => {return {id: value.asin}});
 
-});
+    let proceso = new Promise(async(resolve, reject)=>{
+        for (let i = 0; i < asin.length; i++) {
+            if(localStorage.getItem(asin[i].id)) continue;
+            let data = await getProductId(asin[i])
+            localStorage.setItem(asin[i].id, JSON.stringify(data))
+        }
+        resolve({message: "Datos buscados correctamente" });
+    })
+    Promise.all([proceso]).then(res => {console.log(res);})
+
+}
+
+addEventListener("DOMContentLoaded", async e=>{
+    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
+    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
+    
+    history.pushState(null, "", "?id=fashion");
+    input__search.value = "zapato"
+    const eventoChange = new Event('change');
+    input__search.dispatchEvent(eventoChange);    
+})
+
+input__search.addEventListener("change", searchProducts);
 
